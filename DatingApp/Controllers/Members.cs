23fd67ab51e1b227a7645b0de50c2f1e0e5fa1ae
upamachy <upamachy360@gmail.com>
2025-08-25 02:56:44 +0600

    Initial

---
+++ DatingApp/Controllers/Members.cs
@@ -0,0 +1,27 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+    {
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class MembersController ( AppDbContext context ) : ControllerBase
+        {
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers ( )
+            {
+            var users = await context.Users.ToListAsync ( );
+            return Ok (users);
+            }
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<AppUser>> GetMember ( string id )
+            {
+            var user =await context.Users.FindAsync (id);
+            if (user == null) return NotFound ( );
+            return Ok (user);
+            }
+        }
+    }
---
+++ DatingApp/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using DatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+    {
+    public class AppDbContext ( DbContextOptions<AppDbContext> options ) : DbContext(options)
+        {
+        public DbSet<AppUser> Users { get; set; }
+        }
+    }
