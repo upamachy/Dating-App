3d2df26504189087a18033ece4a884e5a5e99d4c
upamachy <upamachy360@gmail.com>
2025-09-11 00:56:47 +0600

    Add AccountController for user registration functionality
    Introduces the `AccountController` class to manage user registration, including methods for registering users and checking email availability. Implements dependency injection for `AppDbContext` to handle database operations. Also adds the `RegisterDto` class to validate user input for display name, email, and password requirements.

---
+++ DatingApp/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using DatingApp.Data;
+using DatingApp.DTOs;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DatingApp.Controllers
+    {
+    public class AccountController(AppDbContext context) : BaseController
+        {
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+            {
+            if (await EmailExist (registerDto.Email)) { return BadRequest("Email Taken"); }
+            using var hmac = new HMACSHA512 ( );
+            var user = new AppUser
+                {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                PasswordHash = hmac.ComputeHash (Encoding.UTF8.GetBytes (registerDto.Password)),
+                PasswordSalt = hmac.Key
+                };
+            context.Users.Add (user); // tracking
+            await context.SaveChangesAsync ( );// commit/save
+
+            return user;
+            }
+        private async Task<bool> EmailExist(string email )
+            {
+             return await context.Users.AnyAsync(x=> x.Email.ToLower() == email.ToLower() );
+            }
+        }
+    }
---
+++ DatingApp/DTOs/RegisterDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.DTOs
+    {
+    public class RegisterDto
+        {
+
+
+        [Required] //specifies that the property is required and cannot be null or empty.
+        public string DisplayName { get; set; } = "";
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = ""; 
+
+        [Required]
+        [MinLength(4)]
+        public string Password { get; set; } = "";
+        }
+    }
